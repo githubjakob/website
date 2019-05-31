908a417622106bb5e9e4152e09861b24b17b5716
Jakob Zanker <jakob.zanker@googlemail.com>
2019-05-31 10:26:03 +0200

    minify html

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 uglifyjs page/js/script.js > page/js/script.min.js
 uglifycss page/css/style.css > page/css/style.min.css
+html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true -o page/index.html page/index-full.html
